23d63b1d8c0d3e11e95202864f8b67a7750d483d
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-05 23:17:21 +0530

    Time: 2 ms (82.20%), Space: 42.3 MB (80.03%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int myAtoi(String s) {
+        Stack<Character> st=new Stack<>();
+        long ans=0;
+        s=s.trim();
+        if(s.length()==0)return 0;
+        if(s.length()==1)return Character.isDigit(s.charAt(0))?s.charAt(0)-'0':0; if(!Character.isDigit(s.charAt(0))&&!Character.isDigit(s.charAt(1)))return 0;
+        if(s.indexOf('-')==0&&s.indexOf('+')==1)return 0;
+        if(!Character.isDigit(s.charAt(0))&&s.charAt(0)!='+'&&s.charAt(0)!='-')return 0;
+        for(int i=0;i<s.length();i++){
+           
+            char ch=s.charAt(i);
+            if(ch=='+'||ch=='-')continue;
+            while(i<s.length()&&s.charAt(i)-'0'==0){
+                i++;
+            }
+            while(i<s.length()&&Character.isDigit(s.charAt(i))){
+               
+                int val=s.charAt(i)-'0';
+                ans=ans*10+val;
+                if(ans>Integer.MAX_VALUE)return s.charAt(0)=='-'?Integer.MIN_VALUE:Integer.MAX_VALUE;
+                i++;
+            }
+            break;
+        }
+        return s.charAt(0)=='-'?-1*(int)ans:(int)ans;
+    }
+}
