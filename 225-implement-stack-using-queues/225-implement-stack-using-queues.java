fd2364e274e267aa9e5f454e33bf46d250dc11dc
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-05 19:38:30 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (28.59%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,23 +1,34 @@
 class MyStack {
-    Deque<Integer> stack=new ArrayDeque<>();
+    Deque<Integer> stack1=new ArrayDeque<>();
+    Deque<Integer> stack2=new ArrayDeque<>();
+    int top=-1;
     public MyStack() {
         
     }
     
     public void push(int x) {
-        stack.addLast(x);
+        stack1.add(x);
+        top=x;
     }
     
     public int pop() {
-        return stack.removeLast();
+        int ans=-1;
+        while(stack1.size()>1){
+            top=stack1.remove();
+            stack2.add(top);
+        }
+        ans=stack1.remove();
+        stack1=stack2;
+        stack2=new ArrayDeque<>();
+        return ans;
     }
     
     public int top() {
-       return stack.getLast();
+       return top;
     }
     
     public boolean empty() {
-        return stack.isEmpty();
+        return stack1.isEmpty();
     }
 }
 
