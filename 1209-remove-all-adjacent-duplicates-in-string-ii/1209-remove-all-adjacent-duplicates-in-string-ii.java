5b194db06466541c42466760da9a0210fe0cfb86
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-06 10:04:04 +0530

    Time: 174 ms (12.33%), Space: 56.1 MB (11.10%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -15,22 +15,21 @@ class Solution {
         //else put curr,1;
         Stack<Pair> st=new Stack<>();
         st.push(new Pair(s.charAt(0),1));
-        for(int i=1;i<s.length();i++){
-            Pair top=st.peek();
-            if(top.freq==k){
-                while(st.size()>0&&st.peek().ch==top.ch){
-                    st.pop();
-                }
-            }
+        for(int i=1;i<s.length();i++){ 
             if(st.size()>0&&s.charAt(i)==st.peek().ch){
                 st.push(new Pair(s.charAt(i),st.peek().freq+1));
             }
             else{
                 st.push(new Pair(s.charAt(i),1));
             }
+            Pair top=st.peek();
+            if(top.freq==k){
+                while(st.size()>0&&st.peek().ch==top.ch){
+                    st.pop();
+                }
+            }
         }
-        char ch=st.peek().ch;
-        if(st.size()>0&&st.peek().freq==k)while(st.size()>0&&st.peek().ch==ch){st.pop();}
+        
         StringBuilder sb=new StringBuilder();
         while(st.size()>0){
             sb.append(st.pop().ch);
