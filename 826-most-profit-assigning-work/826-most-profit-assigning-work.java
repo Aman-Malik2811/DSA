cf341dc5093495979671ccf4fb3ee4df42872841
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-06 13:03:36 +0530

    Time: 23 ms (76.83%), Space: 61 MB (53.08%) - LeetHub

--- 826-most-profit-assigning-work/826-most-profit-assigning-work.java
+++ 826-most-profit-assigning-work/826-most-profit-assigning-work.java
@@ -13,30 +13,14 @@ class Solution {
             jobs[i]=new Pair(difficult[i],profit[i]);
         }
         Arrays.sort(jobs,(j1,j2)->j1.diff-j2.diff);
-        int[] maxProfit=new int[jobs.length];
-        maxProfit[0]=jobs[0].pro;
-        for(int i=1;i<jobs.length;i++){
-            maxProfit[i]=Math.max(jobs[i].pro,maxProfit[i-1]);
-        }
-       
-        int ans=0;
+        Arrays.sort(worker);
+        int ans=0,i=0,temp=0;
         for(int emp:worker){
-            int i=0,j=jobs.length-1;
-            int temp=-1;
-            while(i<=j){
-                int mid=i+(j-i)/2;
-                if(jobs[mid].diff>emp){
-                    j=mid-1;
-                }
-                else{
-                    while(mid<jobs.length&&jobs[mid].diff<=emp){
-                        temp=mid;
-                        mid++;
-                    }
-                    break;
-                }
+            while(i<jobs.length&&jobs[i].diff<=emp){
+                temp=Math.max(temp,jobs[i].pro);
+                i++;
             }
-            ans+=temp>=0?maxProfit[temp]:0;
+            ans+=temp;
         }
         return ans;
     }
