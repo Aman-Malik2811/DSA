9d600183f54b0bcd4f05b7c4989919d9d59f3dd6
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-06 10:10:49 +0530

    Time: 174 ms (12.33%), Space: 56.1 MB (11.10%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -17,21 +17,22 @@ class Solution {
         st.push(new Pair(s.charAt(0),1));
         for(int i=1;i<s.length();i++){ 
             if(st.size()>0&&s.charAt(i)==st.peek().ch){
-                st.peek().freq++;
+                st.push(new Pair(s.charAt(i),st.peek().freq+1));
             }
             else{
                 st.push(new Pair(s.charAt(i),1));
             }
             Pair top=st.peek();
             if(top.freq==k){
-                st.pop();
+                while(st.size()>0&&st.peek().ch==top.ch){
+                    st.pop();
+                }
             }
         }
         
         StringBuilder sb=new StringBuilder();
         while(st.size()>0){
-           Pair top=st.pop();
-            while(top.freq-->0)sb.append(top.ch);
+            sb.append(st.pop().ch);
         }
         return sb.reverse().toString();
     }
