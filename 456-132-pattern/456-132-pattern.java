4d570fc57046da6ee9607082825a4ff92f8491ef
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-07 18:51:47 +0530

    Time: 42 ms (40.85%), Space: 67.2 MB (36.50%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -5,14 +5,27 @@ class Solution {
           //2->Min of every smallest value to left
           //If for any i nums[i]>right and nums[i]>left and right>left return true;
           // else return false;
-        Stack <Integer> stack = new Stack ();
-        int second = Integer.MIN_VALUE;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums [i] < second)
-                return true;
-            while (!stack.isEmpty() && nums [i] > stack.peek ())
-                second = stack.pop ();
-            stack.push (nums [i]);
+        int n=nums.length;
+        int[] left=new int[n];
+        left[0]=nums[0];
+        for(int i=1;i<n-1;i++){
+            left[i]=Math.min(left[i-1],nums[i-1]);
+        }
+        Stack<Integer> st=new Stack<>();
+        st.push(nums[n-1]);
+        for(int i=n-2;i>=1;i--){
+            int temp=Integer.MIN_VALUE;
+            while(st.size()>0&&nums[i]>st.peek()){
+                temp=Math.max(temp,st.pop());
+            }
+            if(temp==Integer.MIN_VALUE){
+                st.push(nums[i]);
+                continue;
+            }
+            else{
+                if(temp>left[i]&&nums[i]>left[i])return true;
+            }
+            st.push(nums[i]);
         }
         return false;
     }
