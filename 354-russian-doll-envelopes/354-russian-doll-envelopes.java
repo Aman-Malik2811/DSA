de56263a1358053fcf340142e913b0b9c7738ad6
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-05-28 18:38:20 +0530

    Time: 57 ms (87.18%), Space: 85.8 MB (18.92%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxEnvelopes(int[][] env) {
-        Arrays.sort(env,(a,b)->a[0]!=b[0]?a[0]-b[0]:b[1]-a[1]);
-        int ans=Integer.MIN_VALUE;
+        if(env==null||env.length==0)return 0;
+        Arrays.sort(env,(a,b)->a[0]!=b[0]?(a[0]-b[0]):(b[1]-a[1]));
         List<int[]> doll=new ArrayList<>();
-        for(int[] en:env){
-            int i=0,j=doll.size();
-            if(doll.size()==0||valid(doll,en)){
-                doll.add(en);
+        for(int i=0;i<env.length;i++){
+            if(doll.size()==0||valid(doll,env[i])){
+                doll.add(env[i]);
                 continue;
             }
-            while(i!=j){
-                int mid=(i+j)/2;
-                int[] curr=doll.get(mid);
-                if(curr[1]>=en[1]){
-                    j=mid;
+            int st=0,en=doll.size();
+            while(st!=en){
+                int mid=(st+en)/2;
+                int[] last=doll.get(mid);
+                if(last[1]>=env[i][1]){
+                    en=mid;
                 }
                 else{
-                    i=mid+1;
+                    st=mid+1;
                 }
             }
-            doll.set(i,en);
+            doll.set(st,env[i]);
         }
         return doll.size();
     }
-    private boolean valid(List<int[]> doll,int[] en){
+     private boolean valid(List<int[]> doll,int[] en){
         int[] last=doll.get(doll.size()-1);
         return last[0]<en[0]&&last[1]<en[1];
     }
