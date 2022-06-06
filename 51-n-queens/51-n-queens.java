c0f96a72b730881beed8bcbd32f93087006c8ed8
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-06 19:46:09 +0530

    Time: 7 ms (48.18%), Space: 47 MB (13.13%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,50 +1,35 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-        List<String> temp=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb=new StringBuilder();
-            for(int j=0;j<n;j++){
-                sb.append(".");
-            }
-            temp.add(sb.toString());
-        }
-        helper(ans,temp,new boolean[n][n],0);
-        return ans;
+   public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        String[] board = new String[n];
+        char[] initial = new char[n];
+        Arrays.fill(initial, '.');
+        Arrays.fill(board, String.valueOf(initial));
+        boolean[] cols = new boolean[n], d1 = new boolean[2 * n], d2 = new boolean[2 * n];
+        helper(res, board, cols, d1, d2, 0, n);
+        return res;
     }
-    private void helper(List<List<String>> ans,List<String> temp,boolean[][] board,int row){
-        if(row==board.length){
-            ans.add(new ArrayList<>(temp));
-            return;
+    public void helper(List<List<String>> res, String[] board, boolean[] cols, boolean[] d1, boolean[] d2, int row, int n){
+        if(row == n){
+            res.add(new ArrayList<String>(Arrays.asList(board)));
+            return ;
         }
-        for(int i=0;i<board.length;i++){
-            if(valid(board,row,i)){
-               char[] curr=temp.get(row).toCharArray();
-               curr[i]='Q';
-               temp.set(row,new String(curr));
-                board[row][i]=true;
-               helper(ans,temp,board,row+1);
-                board[row][i]=false;
-               curr[i]='.';
-               temp.set(row,new String(curr));
-            }
-        } 
-    }
-    private boolean valid(boolean[][] board,int row,int col){
-        int temp=row;
-        for(int i=0;i<row;i++){
-            if(board[i][col])return false;
+        
+        for(int col = 0; col < n; col ++){
+            if(cols[col] || d1[row + col] || d2[row - col + n]) continue;
+            
+            cols[col] = true; d1[row + col] = true; d2[row - col + n] = true;
+            char[] cur = board[row].toCharArray();
+            cur[col] = 'Q';
+            board[row] = String.valueOf(cur);
+            
+            helper(res, board, cols, d1, d2, row + 1, n);
+            
+            cur = board[row].toCharArray();
+            cur[col] = '.';
+            board[row] = String.valueOf(cur);
+            
+            cols[col] = false; d1[row + col] = false; d2[row - col + n] = false;
         }
-        row--;
-        for(int i=col-1;i>=0&&row>=0;i--){
-            if(board[row][i])return false;
-            row--;
-        }
-        row=temp-1;
-        for(int i=col+1;i<board.length&&row>=0;i++){
-            if(board[row][i])return false;
-            row--;
-        }
-        return true;
     }
 }
