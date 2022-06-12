84e03dc7bac23c986611834fac6a4db99c2edc59
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-12 22:35:24 +0530

    Time: 152 ms (14.03%), Space: 120.2 MB (52.41%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -6,20 +6,19 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             prefix[i]=prefix[i-1]+nums[i];
         }
-        Map<Integer,Integer> map=new HashMap<>();
+        Set<Integer> set=new HashSet<>();
         int max=nums[0];
-        int curr=nums[0];
+        int curr=0;
         int lef=0;
-        map.put(nums[0],0);
-        for(int i=1;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                lef=Math.max(lef,map.get(nums[i]));
-                curr=prefix[i]-prefix[lef];
-                map.put(nums[i],i);
+        for(int i=0;i<nums.length&&lef<nums.length;){
+            if(set.contains(nums[i])){
+                curr-=nums[lef];
+                set.remove(nums[lef]);
+                lef++;
             }
             else{
-                map.put(nums[i],i);
                 curr+=nums[i];
+                set.add(nums[i++]);
             }
             max=Math.max(max,curr);
         }
