86088c2f988ab514d59eb90c01b916400154e732
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-24 00:00:41 +0530

    Time: 4 ms (24.80%), Space: 44.4 MB (31.65%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,19 +1,18 @@
 class Solution {
      public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-         Arrays.sort(nums);
-        helper(ans,new ArrayList<>(),nums,0);
-        return ans;
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
     }
-    private void helper(List<List<Integer>> ans,List<Integer> temp,int[] nums,int st){
-        if(st==nums.length){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-        temp.add(nums[st]);
-        helper(ans,temp,nums,st+1);
-        temp.remove(temp.size()-1);
-        while(st+1<nums.length&&nums[st+1]==nums[st])st++;
-        helper(ans,temp,nums,st+1);
-    }
+} 
 }
