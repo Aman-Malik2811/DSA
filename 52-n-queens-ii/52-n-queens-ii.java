127a56011f79351cfd2bab524e5043c19fa17747
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-25 19:10:44 +0530

    Time: 4 ms (49.32%), Space: 42.9 MB (6.73%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -5,35 +5,44 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<n;i++)sb.append(".");
         for(int i=0;i<n;i++)temp.add(sb.toString());
-        helper(temp,n,0);
+        boolean[] col=new boolean[n];
+        boolean[] upLeftDiag=new boolean[2*n-1];
+        boolean[] upRightDiag=new boolean[2*n-1];
+        helper(temp,n,0,col,upLeftDiag,upRightDiag);
         return count;
     }
-    private boolean valid(List<String> board,int row,int col){
-        //currCol
-        for(int i=0;i<row;i++){
-            if(board.get(i).charAt(col)=='Q')return false;
-        }
-        //leftDiagonal
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if(board.get(i).charAt(j)=='Q')return false;
-        }
-        //rightDiagonal
-        for(int i=row-1,j=col+1;i>=0&&j<board.size();i--,j++){
-            if(board.get(i).charAt(j)=='Q')return false;
-        }
-        return true;
-    }
-    private void helper(List<String> board,int n,int row){
+   // // private boolean valid(List<String> board,int row,int col){
+   //      //currCol
+   //      for(int i=0;i<row;i++){
+   //          if(board.get(i).charAt(col)=='Q')return false;
+   //      }
+   //      //leftDiagonal
+   //      for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+   //          if(board.get(i).charAt(j)=='Q')return false;
+   //      }
+   //      //rightDiagonal
+   //      for(int i=row-1,j=col+1;i>=0&&j<board.size();i--,j++){
+   //          if(board.get(i).charAt(j)=='Q')return false;
+   //      }
+   //      return true;
+   //  }
+    private void helper(List<String> board,int n,int row,boolean[] col,boolean[] left,boolean[] right){
         if(row==n){
             count++;
             return;
         }
         for(int i=0;i<n;i++){
-            if(valid(board,row,i)){
+            if(!col[i]&&!left[i+row]&&!right[row+n-i-1]){
                 char[] currRow=board.get(row).toCharArray();
                 currRow[i]='Q';
                 board.set(row,new String(currRow));
-                helper(board,n,row+1);
+                col[i]=true;
+                left[i+row]=true;
+                right[row+n-i-1]=true;
+                helper(board,n,row+1,col,left,right);
+                col[i]=false;
+                left[i+row]=false;
+                right[row+n-i-1]=false;
                 currRow[i]='.';
                 board.set(row,new String(currRow));
             }
