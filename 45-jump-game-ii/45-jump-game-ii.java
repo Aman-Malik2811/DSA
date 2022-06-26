e9bc84792adb0f7aa47755eed2a87b9091dde3ab
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-26 20:56:12 +0530

    Time: 2 ms (78.75%), Space: 49.3 MB (58.90%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,20 +1,28 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp=new int[nums.length];
-        int n=nums.length;
-        Arrays.fill(dp,-1);
-        dp[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            int jumps=nums[i];
-            if(jumps==0){dp[i]=10001;continue;}
-            int j=1;
-            int min=Integer.MAX_VALUE;
-            while(i+j<n&&jumps-->0){
-                min=Math.min(min,dp[i+j]);
-                j++;
+        // int[] dp=new int[nums.length];
+        // int n=nums.length;
+        // dp[n-1]=0;
+        // for(int i=n-2;i>=0;i--){
+        //     int jumps=nums[i];
+        //     if(jumps==0){dp[i]=10001;continue;}
+        //     int j=1;
+        //     int min=Integer.MAX_VALUE;
+        //     while(i+j<n&&jumps-->0){
+        //         min=Math.min(min,dp[i+j]);
+        //         j++;
+        //     }
+        //     dp[i]=1+min;
+        // }
+        // return dp[0];
+        int jumps=0,currEnd=0,maxEnd=0;
+        for(int i=0;i<nums.length-1;i++){
+            maxEnd=Math.max(maxEnd,i+nums[i]);
+            if(i==currEnd){
+                jumps++;
+                currEnd=maxEnd;
             }
-            dp[i]=1+min;
         }
-        return dp[0];
+        return jumps;
     }
 }
