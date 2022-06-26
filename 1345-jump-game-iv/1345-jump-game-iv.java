be4e64f66a385dda23f7bf98c38d711ffd16b541
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-26 22:29:33 +0530

    Time: 182 ms (25.99%), Space: 125.8 MB (40.50%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,48 +1,29 @@
 class Solution {
     public int minJumps(int[] arr) {
-        int n=arr.length;
-        Map<Integer,List<Integer>> graph=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(graph.containsKey(arr[i])){
-                List<Integer> temp=graph.get(arr[i]);
-                temp.add(i);
-                graph.put(arr[i],temp);
-            }
-            else{
-                List<Integer> list=new ArrayList<>();
-                list.add(i);
-                graph.put(arr[i],list);
-            }
-        }
-        List<Integer> curr=new ArrayList<>();
-        curr.add(0);
-        int step=0;
-        Set<Integer> visited=new HashSet<>();
-        while(!curr.isEmpty()){
-            int count=curr.size();
-            List<Integer> next=new ArrayList<>();
-            for(int i=0;i<count;i++){
-                int node=curr.get(i);
-                if(node==n-1)return step;
-                for(int nbr:graph.get(arr[node])){
-                    if(!visited.contains(nbr)){
-                        visited.add(nbr);
-                        next.add(nbr);
+        int n = arr.length;
+        HashMap<Integer, List<Integer>> indicesOfValue = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            indicesOfValue.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
+        boolean[] visited = new boolean[n]; visited[0] = true;
+        Queue<Integer> q = new LinkedList<>(); q.offer(0);
+        int step = 0;
+        while (!q.isEmpty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int i = q.poll();
+                if (i == n - 1) return step; // Reached to last index
+                List<Integer> next = indicesOfValue.get(arr[i]);
+                next.add(i - 1); next.add(i + 1);
+                for (int j : next) {
+                    if (j >= 0 && j < n && !visited[j]) {
+                        visited[j] = true;
+                        q.offer(j);
                     }
                 }
-                graph.get(arr[node]).clear();
-                if(node-1>=0&&!visited.contains(node-1)){
-                    visited.add(node-1);
-                    next.add(node-1);
-                }
-                if(node+1<n&&!visited.contains(node+1)){
-                    visited.add(node+1);
-                    next.add(node+1);
-                }
+                //very crucial step as next contains all occurences of same number and we add all of them in queue at once so no need to check next again;
+                next.clear(); // avoid later lookup indicesOfValue arr[i]
             }
             step++;
-            curr=next;
         }
-        return -1;
+        return 0;
     }
 }
