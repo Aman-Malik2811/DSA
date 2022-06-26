88ac24688d5cef1f2100cb4d5ec98f0fd6a343fa
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-06-26 11:38:44 +0530

    Time: 57 ms (100.00%), Space: 99.1 MB (100.00%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.java
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maximumsSplicedArray(int[] nums1, int[] nums2) {
+        int n=nums1.length;
         int sum1=0,sum2=0;
         for(int val:nums1)sum1+=val;
         for(int val:nums2)sum2+=val;
-        return Math.max(helper(nums1,nums2)+sum1,helper(nums2,nums1)+sum2);
-    }
-    private int helper(int[] nums1,int[] nums2){
-        int curr=0,ans=0;
-        for(int i=0;i<nums1.length;i++){
-            curr=Math.max(nums2[i]-nums1[i],curr+nums2[i]-nums1[i]);
-            ans=Math.max(ans,curr);
+        int[][] dp=new int[n][2];
+        int[][] dp2=new int[n][2];
+        dp[0][0]=nums2[0]-nums1[0];
+        dp[0][1]=0;
+        dp2[0][0]=nums1[0]-nums2[0];
+        dp2[0][1]=0;
+        for(int i=1;i<n;i++){
+            int diff1=nums2[i]-nums1[i];
+            int diff2=-diff1;
+            dp[i][0]=Math.max(dp[i-1][0]+diff1,diff1);
+            dp[i][1]=Math.max(dp[i-1][0],dp[i-1][1]);
+            dp2[i][0]=Math.max(dp2[i-1][0]+diff2,diff2);
+            dp2[i][1]=Math.max(dp2[i-1][0],dp2[i-1][1]);
         }
-        return ans;
+        int cu1=Math.max(dp[n-1][0],dp[n-1][1]);
+        int cu2=Math.max(dp2[n-1][0],dp2[n-1][1]);
+        return Math.max(cu1+sum1,cu2+sum2);
     }
 }
