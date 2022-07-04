f74dbbcddf05709d7670ffca2875b3ff6e59e142
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-07-04 17:31:03 +0530

    Time: 52 ms (9.96%), Space: 54.4 MB (5.48%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] r) {
+       int[] temp=new int[r.length];
+       Arrays.fill(temp,1);
+       PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+       Set<Integer> st=new HashSet<>();
+       int i=0;
+       for(int val:r){pq.add(new int[]{val,i});i++;};
+       while(pq.size()>0){
+           int[] rem=pq.remove();
+           int id=rem[1],a=0,b=0;
+           if(id+1<r.length&&r[id+1]<r[id]){
+               a=temp[id+1];
+           }
+           if(id-1>=0&&r[id-1]<r[id]){
+               b=temp[id-1];
+           }
+           temp[id]=Math.max(a,b)+1;
+       }
+       int ans=0;
+       for(int val:temp){ans+=val;}
+       return ans;
+    }
+}
