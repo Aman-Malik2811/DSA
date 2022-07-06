1fddd6634159da9e7633bda04b77d3c569338ce7
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-07-06 22:18:05 +0530

    Time: 1 ms (45.21%), Space: 41.5 MB (13.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,10 @@
 class Solution {
+    Map<Integer,Integer> st=new HashMap<>();
     public int fib(int n) {
         if(n==0||n==1)return n;
-        return fib(n-1)+fib(n-2);
+        if(st.containsKey(n))return st.get(n);
+        int val=fib(n-1)+fib(n-2);
+        st.put(n,val);
+        return val;
     }
 }
