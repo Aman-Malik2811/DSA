1731283781270b84b41d79a76c85d070c1e79bf9
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-07-06 22:45:42 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (92.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -2,17 +2,13 @@ class Solution {
     public void nextPermutation(int[] nums) {
        int j=nums.length-1;
        while(j>=1&&nums[j-1]>=nums[j])j--;
-       if(j==0){
-           reverse(nums,j,nums.length-1);
-       }
-       else{
+       if(j!=0){
            int id=j-1;
            int en=nums.length-1;
            while(en>=id&&nums[id]>=nums[en])en--;
            swap(nums,id,en);
-           reverse(nums,j,nums.length-1);
        }
-    
+       reverse(nums,j,nums.length-1);
     }
     private void reverse(int[] nums,int st,int en){
         while(st<en){
@@ -27,70 +23,3 @@ class Solution {
         nums[en]=temp;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // int idx=nums.length-1;
- //        for(int i=nums.length-2;i>=0;i--){
- //            if(nums[i]>=nums[i+1])continue;
- //            else{
- //                idx=i;
- //                break;
- //            }
- //        }
- //        if(idx==nums.length-1){
- //            int i=0,j=nums.length-1;
- //            while(i<j){
- //                swap(nums,i,j);
- //                i++;
- //                j--;
- //            }
- //        }
- //        int en=nums.length-1;
- //        while(en>idx){
- //            if(nums[en]>nums[idx]){
- //                swap(nums,en,idx);
- //                break;
- //            }
- //            en--;
- //        }
- //        int st=idx+1,end=nums.length-1;
- //        while(st<end){
- //            swap(nums,st,end);
- //            st++;
- //            end--;
- //        }
