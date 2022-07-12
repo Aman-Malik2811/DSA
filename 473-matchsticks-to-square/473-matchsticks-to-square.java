480edaabbf391360819947178bcce2e8321fcd10
Aman-Malik2811 <103933456+Aman-Malik2811@users.noreply.github.com>
2022-07-12 22:57:38 +0530

    Time: 312 ms (17.36%), Space: 42.2 MB (33.03%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean makesquare(int[] arr) {
+        Arrays.sort(arr);
         int max=0;
         for(int val:arr)max+=val;
-        Arrays.sort(arr);
-        int i=0,j=arr.length-1;
-        while(i<j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-        }
         if(max%4!=0)return false;
-        return helper(arr,new int[]{0,0,0,0},0,max/4);
+        return helper(arr,new int[]{0,0,0,0},arr.length-1,max/4);
     }
     private boolean helper(int[] arr,int[] sides,int i,int max){
-        if(i==arr.length){
+        if(i<0){
             if(sides[0]==sides[1]&&sides[1]==sides[2]&&sides[2]==sides[3]){
                 return true;
             }
@@ -23,7 +15,7 @@ class Solution {
         }
         for(int c=0;c<4;c++){
             sides[c]+=arr[i];
-            if(sides[c]<=max&&helper(arr,sides,i+1,max)){
+            if(sides[c]<=max&&helper(arr,sides,i-1,max)){
                 return true;
             }
             sides[c]-=arr[i];
